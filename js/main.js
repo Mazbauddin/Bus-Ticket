7223e92f36aa57a7819b2d6ba051217559637327
Mazbauddin <mazbauddin2010@gmail.com>
2024-02-19 14:59:18 +0600

    design complete

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
+const allBtn = document.getElementsByClassName("seat-btn");
 
+let count = 0;
+for (const btn of allBtn) {
+  btn.addEventListener("click", function (e) {
+    count = count + 1;
+    document.getElementById("seat-count").innerText = count;
+  });
+}
